6de0d367d210d35a6a4a50b51fc3d987ea0dc270
Hoa Bui <hoabuisla123@gmail.com>
2018-01-08 20:02:15 +0700

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/Show'
+
   get 'users/new'
 
   root "static_pages#home"
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
